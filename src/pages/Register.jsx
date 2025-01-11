03d94fb5ad7792689cee9b88e6779f7f5807ca8a
Mohamed Aneddame <maneddam@e1r8p12.1337.ma>
2025-01-11 17:34:10 +0100

    remove container class

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -4,7 +4,7 @@ import RegisterCard from '../components/register/RegisterCard'
 function Register() {
 	return (
 		<>
-			<div style={{ minHeight: "100vh" }} className="container bg-[#FEDCC5] flex flex-col md:flex md:flex-row-reverse md:justify-around bg-[url('/src/assets/bird_bg.png')] bg-cover bg-left bg-fixed items-center">
+			<div style={{ minHeight: "100vh" }} className="bg-[#FEDCC5] flex flex-col md:flex md:flex-row-reverse md:justify-around bg-[url('/src/assets/bird_bg.png')] bg-cover bg-left bg-fixed items-center">
 				<img src="/src/assets/bird.png" alt="bird" className='animate-float w-[32rem] object-contain md:block hidden' />
 				<RegisterCard />
 			</div>
